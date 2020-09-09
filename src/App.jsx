748545bcfd473d2c02ae86407ed90f9d76ec4238
zsolt <rappizsolt@gmail.com>
2020-09-09 10:28:09 +0200

    Add redirect to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import ViewDiscs from './components/discs/pages/ViewDiscs';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Navbar from './components/nav/Navbar';
 import ViewArtists from './components/artists/pages/ViewArtists';
 import ViewStyles from './components/styles/pages/ViewStyles';
@@ -13,6 +13,7 @@ function App() {
         <Navbar />
         <div className="container-fluid">
           <Switch>
+            <Redirect exact from="/" to="/discs" />
             <Route path="/discs" render={() => <ViewDiscs />} />
             <Route path="/artists" render={() => <ViewArtists />} />
             <Route path="/styles" render={() => <ViewStyles />} />
